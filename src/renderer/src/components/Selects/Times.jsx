07035cf192cceba4930021d55693dadfbeb6c8b1
louis0113 <luizhenrique011304@gmail.com>
2026-03-30 18:05:50 -0300

    feat: tray icon

--- src/renderer/src/components/Selects/Times.jsx
+++ src/renderer/src/components/Selects/Times.jsx
@@ -1,10 +1,10 @@
 import Select from '../Template/Select'
 
 const timesOptions = [
-  { id: 1, value: '1', text: '1 loop' },
-  { id: 2, value: '2', text: '2 loops' },
-  { id: 3, value: '3', text: '3 loops' },
-  { id: 4, value: '4', text: '4 loops' }
+  { id: 1, value: '1', text: '1' },
+  { id: 2, value: '2', text: '2' },
+  { id: 3, value: '3', text: '3' },
+  { id: 4, value: '4', text: '4' }
 ]
 
 function Times({ value, onChange, disabled }) {
