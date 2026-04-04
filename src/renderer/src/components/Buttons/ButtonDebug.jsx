fac3ca770f50bbcf7ea0c0368cee7ce13dd5dae7
Luiz F <luiz.f.1@proton.me>
2026-04-04 10:08:56 -0300

    fix: menu buttons

--- src/renderer/src/components/Buttons/ButtonDebug.jsx
+++ src/renderer/src/components/Buttons/ButtonDebug.jsx
@@ -1,5 +1,25 @@
 import Button from '../Template/Button'
 const api = window.menuAPI
+
 export default function ButtonDebug() {
-  return <Button className="btn-settings" value="Debug" func={() => api.send('debug')} />
+  return (
+    <Button
+      className="btn-settings"
+      value="Debug"
+      menuOptions={[
+        {
+          label: 'Abrir console',
+          onClick: () => api.send('debug'),
+        },
+        {
+          label: 'Resetar',
+          onClick: () => api.send('reload'),
+        },
+        {
+          label: 'Forçar resetar',
+          onClick: () => api.send('force-reload'),
+        },
+      ]}
+    />
+  )
 }
--- src/renderer/src/components/Buttons/ButtonForceReload.jsx
+++
@@ -1,7 +0,0 @@
-import Button from '../Template/Button'
-const api = window.menuAPI
-export default function ButtonForceReload() {
-  return (
-    <Button className="btn-settings" value="Forçar resetar" func={() => api.send('force-reload')} />
-  )
-}
