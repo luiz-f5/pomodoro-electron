b73987ec1277ca5744afdd5ac47272bd8620fdb2
louis0113 <luizhenrique011304@gmail.com>
2026-04-06 17:30:11 -0300

    fix: Notification

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -1,22 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
-import { loadEnv } from 'vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
+export default defineConfig(() => {
   return {
     main: {
       plugins: [externalizeDepsPlugin()],
-      define: {
-        'import.meta.env.VITE_DATABASE_PG': JSON.stringify(env.VITE_DATABASE_PG),
-        'import.meta.env.VITE_USER_PG': JSON.stringify(env.VITE_USER_PG),
-        'import.meta.env.VITE_PASS_PG': JSON.stringify(env.VITE_PASS_PG),
-        'import.meta.env.VITE_HOST_PG': JSON.stringify(env.VITE_HOST_PG),
-        'import.meta.env.VITE_PORT_PG': JSON.stringify(env.VITE_PORT_PG),
-        'import.meta.env.VITE_FREESOUND_TOKEN': JSON.stringify(env.VITE_FREESOUND_TOKEN)
-      },
       build: {
         rollupOptions: {
           external: ['path', 'child_process']
